b6b843ddeec8c0c37844c274d98d7ccc646cfb0b
elky <elkykim@gmail.com>
2020-07-18 23:03:38 +0900

    feed notifier. rss url이 적힌 json을 foreach 하면서 돌다가 최신 글을 발견하면 slack으로 api hook을 날려주는 알리미.

--- index.js
+++ index.js
@@ -1,32 +1,74 @@
-const open = require('open');
-const notifier = require('node-notifier');
-
+var express = require('express');
+var app = express();
+var bodyParser  = require('body-parser');
+var axios = require('axios')
+var fs = require('fs');
+var _ = require('lodash');
 let Parser = require('rss-parser');
 let parser = new Parser();
+var moment = require('moment')
 
-// String
-//notifier.notify('Message');
+var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+console.log("config: " + JSON.stringify(config));
 
-(async () => {
+var sourcesRss = JSON.parse(fs.readFileSync('./feed.json', 'utf8'));
 
-	let feed = await parser.parseURL('https://elky84.github.io/feed.xml');
-	console.log(feed.title);
+function pollFeed() {
+    try {
+        const hook = config["hook"];
+        var latestTime;
+        
+        try {
+            var latest = fs.readFileSync('./latest.json', 'utf8');
+            latestTime = latest['time']
+        }
+        catch(err) {
+            latestTime = moment().subtract(1, 'day')
+        }
 
-	feed.items.forEach(item => {
-		//console.log(item.title + ':' + item.link)
-		notifier.notify({
-			title: item.title,
-			message: item.link,
-			sound: true,
-			wait: true
-		}, function (err, response) {
-			if (err != null && err != "") {
-				console.log(err)
-			} else {
-				console.log(item.title + ':' + item.link)
-				open(item.link);
-			}
-		});
-	});
+        _.forEach(sourcesRss["sources-rss"], function(source) {
+            // console.log(source);
+    
+            parser.parseURL(source.url, function(err, feed) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
 
-})();
+                console.log(feed.title);
+
+                var messages = []
+                feed.items.forEach(item => {
+                    // console.log(item.title + ':' + item.link)
+                    if(latestTime > moment(item.pubDate)) {
+                        return false;
+                    }
+
+                    messages.push('[' + item.title + '] ' + item.link + ' <' + moment(item.pubDate).format('YYYY-MM-DD HH:mm') + '>');
+                });
+
+                if(messages.length <= 0) {
+                    return;
+                }
+
+                message = {"text": messages.join("\n"), "username": feed.title, "icon_url": hook.icon_url, "channel": hook.channel}
+                axios.post(hook.hook_url, message).then((result) => {
+                    console.log(result);
+                });
+
+            });
+        });
+
+        fs.writeFileSync('./latest.json', JSON.stringify({time: moment()}));
+    }
+    catch(e) {
+        console.log(e);
+    }
+
+}
+
+// 우선 한번 실행하고
+pollFeed();
+
+// interval단위 실행
+setInterval(pollFeed, 3 * 60 * 1000);
