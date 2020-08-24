91563987a8832379b22008008df2f52848287a73
elky <elkykim@gmail.com>
2020-08-24 23:46:19 +0900

    latest 파일명에 공백을 _로 변경

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function pollFeed() {
 
                 console.log(feed.title);
 
-                let latestFile = `./${latestDir}/${feed.title}.json`;
+                let latestFile = `./${latestDir}/${feed.title.replace(' ', '_')}.json`;
                 try {
                     latest = JSON.parse(fs.readFileSync(latestFile, 'utf8'));
                 }
